ea72b9d311745730fd945815e880ad64923990de
LizaZayats <liza2007zayats@gmail.com>
2025-10-15 10:50:33 +0300

    Convert A Hex String To RGB

---
+++ Convert A Hex String To RGB.js
@@ -0,0 +1,10 @@
+function hexStringToRGB(hexString) {
+
+    const h = hexString.replace('#', '').toUpperCase();
+    
+    const r = parseInt(h.substring(0, 2), 16);
+    const g = parseInt(h.substring(2, 4), 16);
+    const b = parseInt(h.substring(4, 6), 16);
+    
+    return { r, g, b };
+}
